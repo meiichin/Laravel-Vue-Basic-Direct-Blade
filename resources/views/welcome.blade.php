499261d63f54cbe0f8a397c1119d8d54c7b10c6a
meiichin <meiichan3017@gmail.com>
2019-03-19 19:04:47 +0700

    [Added] vue cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
         <!-- Styles -->
         <style>
